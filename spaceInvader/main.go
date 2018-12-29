516f33939f4448dd4e3db865d9f9ecbd262aed59
Théo <theo.lannoye.etu@univ-lille.fr>
2018-12-29 18:42:26 +0100

    First commit

---
+++ spaceInvader/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"../github.com/faiface/pixel"
+	"../github.com/faiface/pixel/pixelgl"
+	"../golang.org/x/image/colornames"
+	"image"
+	_ "image/png"
+	"os"
+)
+
+func run(){
+	/* Create the window */
+	cfg := pixelgl.WindowConfig{
+		Title:  "Space Invader - Théo Lannoye",
+		Bounds: pixel.R(0,0, 1024, 768),
+		VSync:true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	/* Load images */
+	vaisseau, err := loadPicture("images/vaisseau.png")
+	if err != nil {
+		panic(err)
+	}
+
+	laser, err := loadPicture("images/laser.png")
+	if err != nil {
+		panic(err)
+	}
+
+	/*invaders, err := loadPicture("images/invader.png")
+	if err != nil {
+		panic(err)
+	}/*
+
+	/* Sprites init */
+	spriteVaisseau := pixel.NewSprite(vaisseau, vaisseau.Bounds())
+	spriteLaser := pixel.NewSprite(laser, laser.Bounds())
+	//spriteInvaders := pixel.NewSprite(invaders, invaders.Bounds())
+
+	win.Clear(colornames.Grey)
+
+	lateralPos := 0.0
+
+	/* Display vaisseau */
+	spriteVaisseau.Draw(win, pixel.IM.Moved(win.Bounds().Moved(pixel.V(lateralPos,-340)).Center ()))
+
+	/* Keyboard actions */
+	for !win.Closed() {
+		if win.Pressed(pixelgl.KeyLeft) {
+			lateralPos--
+			if lateralPos >= -450.0 {
+				win.Clear(colornames.Grey)
+				spriteVaisseau.Draw(win, pixel.IM.Moved(win.Bounds().Moved(pixel.V(lateralPos,-340)).Center ()))
+			} else {
+				lateralPos += 20
+				win.Clear(colornames.Grey)
+				spriteVaisseau.Draw(win, pixel.IM.Moved(win.Bounds().Moved(pixel.V(lateralPos,-340)).Center ()))
+			}
+		}
+		if win.Pressed(pixelgl.KeyRight) {
+			lateralPos++
+			if lateralPos <= 450 {
+				win.Clear(colornames.Grey)
+				spriteVaisseau.Draw(win, pixel.IM.Moved(win.Bounds().Moved(pixel.V(lateralPos,-340)).Center ()))
+			} else {
+				lateralPos -= 20
+				win.Clear(colornames.Grey)
+				spriteVaisseau.Draw(win, pixel.IM.Moved(win.Bounds().Moved(pixel.V(lateralPos,-340)).Center ()))
+			}
+		}
+
+		/* Max height went from -340 to 340. Begin the laser at -265 to simulate a shoot */
+		if win.JustPressed(pixelgl.KeySpace){
+			spriteLaser.Draw(win, pixel.IM.Moved(win.Bounds().Moved(pixel.V(lateralPos, -265.0)).Center ()))
+			for i := -265.0; i<=340; i++ {
+				spriteLaser.Draw(win, pixel.IM.Moved(win.Bounds().Moved(pixel.V(lateralPos, i)).Center ()))
+			}
+		}
+
+		win.Update()
+	}
+}
+
+func main(){
+	pixelgl.Run(run)
+}
+
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
